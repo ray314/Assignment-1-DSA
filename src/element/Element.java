003b11975985db95e849aa7c13eab2ca984f2754
Queck <yeungj1602@gmail.com>
2019-08-10 23:25:13 +1200

    Organize imports

--- src/element/Element.java
+++ src/element/Element.java
@@ -4,11 +4,7 @@
 package element;
 
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
 
 /**
  * @author Justin Yeung
